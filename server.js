872eccfa87cc988231fd3c36be074d4a7fa2c44c
Atuoha <atuohainitiatives@gmail.com>
2020-09-30 05:32:20 -0700

    handlebars template engine inits-:

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const server = express();
+const path = require('path');
+// const handlebars = require('express-handlebars')
 const port = 1111 || process.env.Port
 
+server.use(express.static(path.join(__dirname, 'public'))) // Loading Static files
+
+// server.engine('handlebars', handlebars({defaultLayout: 'home'}))
+// server.set('view engine', 'handlebars')
+
 server.get('/', (req, res)=>{
-    res.send('Home')
+    // res.render('home/index')
+})
+
+server.get('/about', (req, res)=>{
+    // res.render('home/about')
+})
+
+server.get('/login', (req, res)=>{
+    // res.render('home/login')
+})
+
+
+server.get('/register', (req, res)=>{
+    // res.render('home/register')
 })
 
 server.listen(port, ()=>{
