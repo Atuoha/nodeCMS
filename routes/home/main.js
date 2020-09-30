95f627d2751add2a83b9d3d66894ab5cb7d6ed96
Atuoha <atuohainitiatives@gmail.com>
2020-09-30 11:46:33 -0700

    express-handler inits:-

---
+++ routes/home/main.js
@@ -0,0 +1,22 @@
+const express =  require('express')
+const router = express.Router();
+
+router.get('/', (req, res)=>{
+    // res.render('home/index')
+})
+
+router.get('/about', (req, res)=>{
+    // res.render('home/about')
+})
+
+router.get('/login', (req, res)=>{
+    // res.render('home/login')
+})
+
+
+router.get('/register', (req, res)=>{
+    // res.render('home/register')
+})
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,30 +1,25 @@
 const express = require('express');
 const server = express();
 const path = require('path');
-// const handlebars = require('express-handlebars')
+const handlebars = require('express-handlebars')
 const port = 1111 || process.env.Port
 
 server.use(express.static(path.join(__dirname, 'public'))) // Loading Static files
 
-// server.engine('handlebars', handlebars({defaultLayout: 'home'}))
-// server.set('view engine', 'handlebars')
-
-server.get('/', (req, res)=>{
-    // res.render('home/index')
-})
-
-server.get('/about', (req, res)=>{
-    // res.render('home/about')
-})
-
-server.get('/login', (req, res)=>{
-    // res.render('home/login')
-})
+// --SETTING view engine using handlebars
+server.engine('handlebars', handlebars({defaultLayout: 'home'}))
+server.set('view engine', 'handlebars')
 
 
-server.get('/register', (req, res)=>{
-    // res.render('home/register')
-})
+// Routes
+// --home
+const home = require('./routes/home/main')
+server.use('/', home)
+
+// --admin
+const admin =  require('/routes/admin/main');
+server.use('/admin', admin)
+// 
 
 server.listen(port, ()=>{
     console.log(`listening to port: ${port}`)
